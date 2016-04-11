aa5c48dccde845e815d1f6e4c5000fe8beb65502
jnf <jeremy.flores@gmail.com>
2016-04-11 16:19:08 -0700

    adds a reset_schema method that will replace the existing schema

--- lib/setup_db.rb
+++ lib/setup_db.rb
@@ -6,6 +6,46 @@ class ReleaseDatabase
   def initialize(dbname = "releases")
     @db = SQLite3::Database.new "database/#{dbname}.db"
   end
+
+  def reset_schema!
+    # table name: albums
+    # label_code,artist,title,label,format,released,date_added
+    # BLOB, TEXT, TEXT, TEXT, TEXT,        INTEGER,     TEXT
+    # one table
+    query = <<-CREATESTATEMENT
+      CREATE TABLE albums (
+        id INTEGER PRIMARY KEY,
+        label_code BLOB,
+        artist TEXT NOT NULL,
+        title TEXT NOT NULL,
+        label TEXT,
+        format TEXT,
+        released INTEGER,
+        date_added TEXT
+      );
+    CREATESTATEMENT
+
+    @db.execute("DROP TABLE IF EXISTS albums;")
+    @db.execute(query) #runs one and only one query!
+  end
 end
 
 release_db = ReleaseDatabase.new
+release_db.reset_schema!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
